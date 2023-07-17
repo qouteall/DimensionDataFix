c9ed1718d9535c018089a5bf474824cf34b82b37
qouteall <qouteall@163.com>
2023-07-17 22:53:55 +0800

    upgrade fix for 1.20.1

--- src/main/java/qouteall/dimension_data_fix/DimensionDataFix.java
+++ src/main/java/qouteall/dimension_data_fix/DimensionDataFix.java
@@ -1,34 +1,17 @@
 package qouteall.dimension_data_fix;
 
-import com.mojang.datafixers.util.Unit;
-import com.mojang.serialization.DataResult;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.nbt.NbtCompound;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DimensionDataFix implements ModInitializer {
-    @Override
-    public void onInitialize() {
-    
-    }
-    
-    public static void purgeDimensionsFromWorldGenSettingsTag(NbtCompound worldGenSettings) {
-        String[] vanillaDimensionIds =
-            new String[]{"minecraft:overworld", "minecraft:the_nether", "minecraft:the_end"};
-        
-        NbtCompound dimensions = worldGenSettings.getCompound("dimensions");
-        
-        if (dimensions.getSize() > 3) {
-            NbtCompound newDimensions = new NbtCompound();
-            for (String dimId : vanillaDimensionIds) {
-                if (dimensions.contains(dimId)) {
-                    newDimensions.put(dimId, dimensions.getCompound(dimId));
-                }
-            }
-            
-            worldGenSettings.put("dimensions", newDimensions);
-            System.out.println("Purged WorldGenSetting dimensions");
-        }
-    }
+    public static final Logger LOGGER = LoggerFactory.getLogger("dimension_data_fix");
+	
+	public static boolean enableUpgradeFix = true;
+	
+	@Override
+	public void onInitialize() {
+	
+	}
 }
--- src/main/java/qouteall/dimension_data_fix/mixin/MixinLevelStorage.java
+++ src/main/java/qouteall/dimension_data_fix/mixin/MixinLevelStorage.java
@@ -1,41 +1,79 @@
 package qouteall.dimension_data_fix.mixin;
 
 import com.mojang.datafixers.DataFixer;
-import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.Dynamic;
-import com.mojang.serialization.Lifecycle;
+import com.mojang.serialization.DynamicOps;
+import net.minecraft.datafixer.DataFixTypes;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
-import net.minecraft.world.gen.GeneratorOptions;
 import net.minecraft.world.level.storage.LevelStorage;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import qouteall.dimension_data_fix.DimensionDataFix;
 
 @Mixin(LevelStorage.class)
 public class MixinLevelStorage {
+    @Shadow
+    @Final
+    private static Logger LOGGER;
     
-    @Inject(
+    @Redirect(
         method = "readGeneratorProperties",
-        at = @At("HEAD")
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/datafixer/DataFixTypes;update(Lcom/mojang/datafixers/DataFixer;Lcom/mojang/serialization/Dynamic;I)Lcom/mojang/serialization/Dynamic;"
+        )
     )
-    private static <T> void onReadGeneratorProperties(
-        Dynamic<T> nbt, DataFixer dataFixer, int version,
-        CallbackInfoReturnable<Pair<GeneratorOptions, Lifecycle>> cir
+    private static <T> Dynamic<T> redirectUpdateToCurrentVersion(
+        DataFixTypes instance, DataFixer dataFixer, Dynamic<T> dynamic, int version
     ) {
-        
-        NbtElement nbtTag = ((Dynamic<NbtElement>) nbt).getValue();
-        
-        if (nbtTag instanceof NbtCompound compoundTag) {
-            NbtCompound worldGenSettings = ((NbtCompound) nbtTag).getCompound("WorldGenSettings");
-            
-            DimensionDataFix.purgeDimensionsFromWorldGenSettingsTag(worldGenSettings);
+        if (!DimensionDataFix.enableUpgradeFix) {
+            return instance.update(dataFixer, dynamic, version);
         }
         
+        NbtCompound value = (NbtCompound) dynamic.getValue();
+        DynamicOps<T> ops = dynamic.getOps();
         
+        LOGGER.debug("Fixing dimension data {}", value);
+        
+        NbtCompound dimensions = value.getCompound("dimensions");
+        
+        NbtCompound vanillaDimensions = new NbtCompound();
+        NbtCompound nonVanillaDimensions = new NbtCompound();
+        
+        for (String dimensionId : dimensions.getKeys()) {
+            NbtElement data = dimensions.get(dimensionId);
+            if (dimensionId.startsWith("minecraft:")) {
+                vanillaDimensions.put(dimensionId, data);
+            }
+            else {
+                nonVanillaDimensions.put(dimensionId, data);
+            }
+        }
+        
+        NbtCompound newValue = value.copy();
+        newValue.put("dimensions", vanillaDimensions);
+        
+        Dynamic<T> dynamicOfVanillaDimensions = (Dynamic<T>) new Dynamic<>(ops, (T) newValue);
+        
+        Dynamic<T> updated = instance.update(dataFixer, dynamicOfVanillaDimensions, version);
+        
+        LOGGER.debug("Updated {}", updated.getValue());
+        
+        NbtCompound updatedTag = (NbtCompound) updated.getValue();
+        NbtCompound updatedDimensions = updatedTag.getCompound("dimensions");
+        
+        for (String nonVanillaDim : nonVanillaDimensions.getKeys()) {
+            NbtElement data = nonVanillaDimensions.get(nonVanillaDim);
+            updatedDimensions.put(nonVanillaDim, data);
+        }
+        
+        LOGGER.debug("Fixed dimension data {}", updatedTag);
+        
+        return updated;
     }
-    
-    
 }
